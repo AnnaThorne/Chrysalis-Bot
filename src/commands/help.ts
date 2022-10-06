a796aaeac5d522123fb5c3d50aeedcd8c989ea23
Annie <at.anna.thorne@gmail.com>
2022-10-06 11:05:08 +0200

    refactoring

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,18 +1,25 @@
 import {SlashCommandBuilder} from "@discordjs/builders";
 import {EmbedBuilder} from "discord.js";
-import { ICommand } from "../interfaces/Command";
+import {ICommand} from "../interfaces/Command";
 
 export const help: ICommand = {
     data: new SlashCommandBuilder()
         .setName("help")
         .setDescription("Pony posting"),
     run: async (interaction) => {
-        if(interaction.isChatInputCommand()) {
+        if (interaction.isChatInputCommand()) {
             await interaction.deferReply();
             const helpEmbed = new EmbedBuilder()
-                .setColor(0x09999).setTitle("Pony Bot")
-                .setDescription("This bot posts ponies. Use /pony to post a pony.")
-                .setFooter({text: `Version ${process.env.npm_package_version}`});
+                .setColor(0x09999).setTitle("Chrysalis Help")
+                .setDescription("Chrysalis is a bot that posts ponies. " +
+                    "It is currently in development, so not all features are available yet. " +
+                    "Here are the commands that are currently available:")
+                .addFields({
+                        name: 'Pony', value: 'Posts a pony. You can specify a pony by name, ' +
+                            'or you can get a random pony by not specifying a name.'
+                    },
+                    {name: 'Help', value: 'Shows this message.'})
+                .setFooter({text: `Version ${process.env.npm_package_version} Developed by Deppy#3350`},);
             await interaction.editReply({embeds: [helpEmbed]});
             return;
         }
