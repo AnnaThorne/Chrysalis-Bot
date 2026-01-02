9a52cd040091ddbd68b4db316e9f6e476b78e30d
Anna Thorne <annathorne@pm.me>
2026-01-02 22:18:45 +0100

    update bot

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,7 +1,14 @@
-import type {SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder} from '@discordjs/builders';
-import type {Interaction} from 'discord.js';
+import type {
+	SlashCommandBuilder,
+	SlashCommandSubcommandsOnlyBuilder,
+	SlashCommandOptionsOnlyBuilder,
+} from "@discordjs/builders";
+import type { Interaction } from "discord.js";
 
 export type Command = {
-	data: Omit<SlashCommandBuilder, 'addSubcommandGroup' | 'addSubcommand'> | SlashCommandSubcommandsOnlyBuilder;
+	data:
+		| Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">
+		| SlashCommandSubcommandsOnlyBuilder
+		| SlashCommandOptionsOnlyBuilder;
 	run: (interaction: Interaction) => Promise<void>;
 };
